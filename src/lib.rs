a2d808c7558621326afd3754dd6a38e7913f6a16
museun <museun@outlook.com>
2019-06-17 18:52:15 -0400

    Add in a lexer that doesn't produce spans
    Lexer was renamed to SpannedLexer, with a new Lexer just returning the
    token

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 //!
 //! ## Example
 //! ```rust
-//! # use logos_iterator::Lexer;
+//! # use logos_iterator::{Lexer, SpannedLexer};
 //! # use logos::Logos;
 //! // see the documentation for Logos on how this derive works
 //! #[derive(Logos, PartialEq, Clone, Copy, Debug)]
@@ -25,42 +25,93 @@
 //!     NewLine,
 //! }
 //!
+//! let input = "1 + 1 = 2;\n2 + 2 = 4;";
+//! let expected = vec![
+//!     Token::Digit,
+//!     Token::Plus,
+//!     Token::Digit,
+//!     Token::Equal,
+//!     Token::Digit,
+//!     Token::End,
+//!     Token::NewLine,
+//!     Token::Digit,
+//!     Token::Plus,
+//!     Token::Digit,
+//!     Token::Equal,
+//!     Token::Digit,
+//!     Token::End,
+//! ];
+//!
 //! // create the iterator from the input source with the token type
-//! // this can generally elide the 'input source', so Lexer::<YourToken, _>
-//! let tokens = Lexer::<Token, &str>::new("1 + 1 = 2;\n2 + 2 = 4;")
-//!     .map(|k| k.item) // remove span information.
-//!                      // each item will be a WithSpan { item, span }
-//!                      // where the span is the byte location (start..end)
-//!                      // of the token in the input source.
+//! // this can generally elide the 'input source', so SpannedLexer::<YourTokenType, _>
+//! let tokens = SpannedLexer::<Token, &str>::new(input)
+//!     .map(|k| k.item) // remove span information. each item will be a `WithSpan { item, span }`
 //!     .collect::<Vec<_>>();
 //!
-//! assert_eq!(
-//!     tokens,
-//!     vec![
-//!         Token::Digit,
-//!         Token::Plus,
-//!         Token::Digit,
-//!         Token::Equal,
-//!         Token::Digit,
-//!         Token::End,
-//!         Token::NewLine,
-//!         Token::Digit,
-//!         Token::Plus,
-//!         Token::Digit,
-//!         Token::Equal,
-//!         Token::Digit,
-//!         Token::End,
-//!     ]
-//! );
+//! assert_eq!(tokens, expected);
+//!
+//! // or without spans
+//! let tokens = Lexer::<Token, _>::new(input).collect::<Vec<_>>();
+//! assert_eq!(tokens, expected);
 //! ```
 
-/// A lexer is an iterator over an input source, `S` that yields token `T` until
+/// A lexer that is an iterator over an input source, `S` that yields token `T` until
 /// the
 /// [`#[logos::end]`](https://docs.rs/logos/latest/logos/trait.Logos.html#associatedconstant.END)
 /// token is found
 ///
 /// The yielded element is wrapped with a [`Span`](./struct.Span.html), which is
 /// the byte offset into the source that the token is located at
+pub struct SpannedLexer<T, S>(::logos::Lexer<T, S>)
+where
+    T: PartialEq<T> + ::logos::Logos;
+
+impl<'a, T, S> SpannedLexer<T, S>
+where
+    T: PartialEq<T> + ::logos::Logos + ::logos::source::WithSource<S>,
+    S: ::logos::source::Source<'a>,
+{
+    /// Create a new lexer from the source `S`
+    ///
+    /// * T is the Token (that
+    ///   [`#[derive(Logos)]`](https://docs.rs/logos/latest/logos/trait.Logos.html)
+    ///   applies to)
+    /// * S is something that implements
+    ///   [`logos::Source`](https://docs.rs/logos/latest/logos/source/trait.Source.html)
+    ///   (`&str`, `&[u8]`, etc)
+    pub fn new(s: S) -> Self {
+        Self(T::lexer(s))
+    }
+}
+
+impl<'a, T, S> Iterator for SpannedLexer<T, S>
+where
+    T: Copy + Clone + PartialEq<T>,
+    T: ::logos::Logos + ::logos::source::WithSource<S>,
+    S: ::logos::source::Source<'a>,
+{
+    type Item = WithSpan<T>;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.0.token == T::END {
+            return None;
+        }
+
+        let token = self.0.token;
+        let range = self.0.range();
+        let span = Span {
+            start: range.start,
+            end: range.end,
+        };
+
+        self.0.advance();
+        Some(WithSpan::new(token, span))
+    }
+}
+
+/// A lexer that is an iterator over an input source, `S` that yields token `T` until
+/// the
+/// [`#[logos::end]`](https://docs.rs/logos/latest/logos/trait.Logos.html#associatedconstant.END)
+/// token is found
 pub struct Lexer<T, S>(::logos::Lexer<T, S>)
 where
     T: PartialEq<T> + ::logos::Logos;
@@ -89,21 +140,15 @@ where
     T: ::logos::Logos + ::logos::source::WithSource<S>,
     S: ::logos::source::Source<'a>,
 {
-    type Item = WithSpan<T>;
+    type Item = T;
     fn next(&mut self) -> Option<Self::Item> {
         if self.0.token == T::END {
             return None;
         }
 
         let token = self.0.token;
-        let range = self.0.range();
-        let span = Span {
-            start: range.start,
-            end: range.end,
-        };
-
         self.0.advance();
-        Some(WithSpan::new(token, span))
+        Some(token)
     }
 }
 
